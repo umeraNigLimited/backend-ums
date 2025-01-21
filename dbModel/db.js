9e69e1be40bed1225d209a35535acce76bd3602e
Apple <apple@Apples-MacBook-Pro.local>
2025-01-21 21:16:58 +0100

    Added gitignore

--- dbModel/db.js
+++ dbModel/db.js
@@ -4,11 +4,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = new pg.Client({
-  // user: "postgres",
-  // host: "127.0.0.1",
-  // database: "umera-staff-dashboard",
-  // password: "Umera2020@2#3$4",
-  // port: "5432",
   connectionString: process.env.DATABASE_URL,
   password: String(process.env.DB_PASSWORD),
 });
